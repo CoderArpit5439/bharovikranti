8892f24b142e7e31cf92f9c5c7d45f1569590e1d
CoderArpit5439 <141112523+CoderArpit5439@users.noreply.github.com>
2023-08-25 13:18:05 +0530

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,9 +1,16 @@
+console.log('custom js loaded');
+
 setTimeout(() {
   document.addEventListener('DOMContentLoaded', function () {
 
+    console.log('DOM LOADED');
+
   var imgElements = document.getElementsByTagName('img');
 
+    console.log(imgElements);
+
   if(imgElements.length > 0) {
+    
     for(let i of imgElements) {
 
   var imgAttr = i.getAttribute('src');
