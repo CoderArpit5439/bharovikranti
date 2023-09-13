7f8a983bf3ab0792411894f27d247a939d05395c
CoderArpit5439 <officialarpit5439@gmail.com>
2023-09-14 01:20:16 +0530

    update marriage web

--- src/pages/Service.js
+++ src/pages/Service.js
@@ -11,7 +11,7 @@ import bkfnn from '../assets/image/bkfnn.jpg'
 const Service = ({ language }) => {
   return (
     <div>
-      <section className="text-gray-400 body-font bg-gradient-to-b from-gray-900 to-gray-400 bg-gradient-to-r">
+      <section className="text-gray-400 body-font bg-gradient-to-t from-gray-700 via-gray-600 to-black">
         <div className="container px-5 py-24 mx-auto">
           <div className="flex flex-wrap w-full mb-20">
             <div className="lg:w-1/2 w-full mb-6 lg:mb-0">
